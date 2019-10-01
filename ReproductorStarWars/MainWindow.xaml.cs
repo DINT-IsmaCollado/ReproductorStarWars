67597e9fbe6db92b9154f87d642d0415c510b8a2
IsmaCollado <ismael.collado@iesdoctorbalmis.com>
2019-10-01 15:01:02 +0200

    Media funcional

--- ReproductorStarWars/MainWindow.xaml.cs
+++ ReproductorStarWars/MainWindow.xaml.cs
@@ -23,6 +23,43 @@ namespace ReproductorStarWars
         public MainWindow()
         {
             InitializeComponent();
+            MediaTrailer.LoadedBehavior = MediaState.Manual;
+        }
+
+        private void ButtonPlay_Click(object sender, RoutedEventArgs e)
+        {
+            string path = Environment.CurrentDirectory;
+            if (RadioButtonTrailer1.IsChecked == true)
+            {
+                MediaTrailer.Source = new Uri(path + @"\trailer1.mp4");
+                MediaTrailer.Play();
+                
+            }
+            if (RadioButtonTrailer2.IsChecked == true)
+            {
+                MediaTrailer.Source = new Uri(path + @"\trailer2.mp4");
+                MediaTrailer.Play();
+            }
+        }
+
+        private void ButtonPause_Click(object sender, RoutedEventArgs e)
+        {
+            MediaTrailer.Pause();
+        }
+
+        private void ButtonStop_Click(object sender, RoutedEventArgs e)
+        {
+            MediaTrailer.Stop();
+        }
+
+        private void CheckBoxMuted_Checked(object sender, RoutedEventArgs e)
+        {
+            MediaTrailer.IsMuted = true;
+        }
+
+        private void CheckBoxMuted_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MediaTrailer.IsMuted = false;
         }
     }
 }
